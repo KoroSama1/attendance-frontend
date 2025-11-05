cbbc7e5d005ec1cc689591af9ca1644aa3a170ab
KoroSama1 <sensaikoro778@gmail.com>
2025-11-05 10:40:40 +0530

    Update API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // change if your backend is elsewhere
+  baseURL: 'https://attendance-backend-6i1v.onrender.com', // change if your backend is elsewhere
   headers: { 'Content-Type': 'application/json' }
 })
 
